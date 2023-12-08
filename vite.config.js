909f6ef98e3076dcef81aaee19a3f2e09027890a
fabrichgit <fabrichjean@gmail.com>
2023-12-08 14:45:32 +0300

    add build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: '/buzz-deploy'
+  build: {
+    base: '/buzz-deploy/'
+  }
 })
 
