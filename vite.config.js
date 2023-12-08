f22f2e9ae4262090c4a159d747b825fd2b2fb160
fabrichgit <fabrichjean@gmail.com>
2023-12-08 14:55:47 +0300

    fix vite.config2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: '/buzz-deploy'
+  base: '/buzz-deploy',
+  build: {
+    outDir: 'dist',
+  },
 })
 
