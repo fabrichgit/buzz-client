125ac4fd3a7416ec4c2ad194128cecabffba22b0
fabrichgit <fabrichjean@gmail.com>
2023-12-08 14:47:51 +0300

    remove build

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  build: {
-    base: '/buzz-deploy/'
-  }
+  base: '/buzz-deploy/'
 })
 
